73a1d0bbb03190f9d06bc32a6af88230de7b46d1
= <tsmit291@icloud.com>
2016-02-09 21:38:14 -0700

    seed files for join table work

--- seeds/seed_joinTable.js
+++ seeds/seed_joinTable.js
@@ -7,35 +7,35 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('jointable').insert({
       books_id: '1',
-      author_id: '1'
+      authors_id: '1'
     }),
     knex('jointable').insert({
       books_id: '1',
-      author_id: '2'
+      authors_id: '2'
     }),
     knex('jointable').insert({
       books_id: '1',
-      author_id: '3'
+      authors_id: '3'
     }),
     knex('jointable').insert({
       books_id: '2',
-      author_id: '4'
+      authors_id: '4'
     }),
     knex('jointable').insert({
       books_id: '3',
-      author_id: '5'
+      authors_id: '5'
     }),
     knex('jointable').insert({
       books_id: '4',
-      author_id: '5'
+      authors_id: '5'
     }),
     knex('jointable').insert({
       books_id: '5',
-      author_id: '5'
+      authors_id: '5'
     }),
     knex('jointable').insert({
       books_id: '6',
-      author_id: '5'
-    }),
-  );
+      authors_id: '5'
+    })
+  )
 };
