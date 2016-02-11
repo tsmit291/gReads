ed141e6626f1e904e4f323f095c22e5b1c36c4b5
= <tsmit291@icloud.com>
2016-02-10 17:09:13 -0700

    done-ish

--- routes/users.js
+++ routes/users.js
@@ -95,7 +95,7 @@ router.post('/books/:id/delete', function (req, res, next){
 router.get('/authors', function(req, res, next){
   authors().select().then(function(results){
     knex.from('books').innerJoin('jointable', 'books.id', 'jointable.books_id').then(function(join){
-      res.render('show', {books: results, joinTable: join});
+      res.render('authors/show', {books: results, joinTable: join});
     });
   });
 });
